e25b0696c87025203960c200cd8eb8c4eb498051
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-07-10 12:24:42 +0700

    u

--- js/main.js
+++ js/main.js
@@ -1,29 +1,6 @@
-$('#header').load('./../Header.html')
-
-// $("button").click(function(){
-//     $.ajax({url: "demo_test.txt", success: function(result){
-//       $("#div1").html(result);
-//     }});
-//   });
 function myFunction(x) {
     x.classList.toggle("change");
 }
-
-function showSearchBar() {
-    document.getElementById("loupeicon").style.width = "7%";
-    document.getElementById("loupeicon").style.zIndex = "99";
-    document.getElementById("loupeicon").style.position = "absolute";
-    document.getElementById("loupeicon").style.backgroundColor = "white";
-}
-
-function hideSearchBar() {
-    document.getElementById("loupeicon").style.width = "";
-    document.getElementById("loupeicon").style.zIndex = "";
-    document.getElementById("loupeicon").style.position = "";
-    document.getElementById("loupeicon").style.backgroundColor = "";
-}
-
-
 
 var imgslider = document.getElementsByClassName("img-slide");
 document.getElementById("slide1").addEventListener("click", () => {
@@ -40,10 +17,10 @@ document.getElementById("slide3").addEventListener("click", () => {
 })
 var index = 0;
 setInterval(() => {
-    $(".img-slide").css("opacity", "0");
-    imgslider[index].style.opacity = "1";
-    index++;
-    if (index == 3) {
-        index = 0;
-    }
-}, 3000)
+        $(".img-slide").css("opacity", "0");
+        imgslider[index].style.opacity = "1";
+        index++;
+        if (index == 3) {
+            index = 0;
+        }
+    }, 5000) //Set time
