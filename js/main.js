fd71cb4d28cdbea90fa8b4d252ba74a714774d80
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-07-10 12:15:30 +0700

    update 10/7

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
-$('#header').load('./../Header.html')
-
-// $("button").click(function(){
-//     $.ajax({url: "demo_test.txt", success: function(result){
-//       $("#div1").html(result);
-//     }});
-//   });
 function myFunction(x) {
     x.classList.toggle("change");
 }
-
-function showSearchBar() {
-    document.getElementById("loupeicon").style.width = "7%";
-    document.getElementById("loupeicon").style.zIndex = "99";
-    document.getElementById("loupeicon").style.position = "absolute";
-    document.getElementById("loupeicon").style.backgroundColor = "white";
-}
-
-function hideSearchBar() {
-    document.getElementById("loupeicon").style.width = "";
-    document.getElementById("loupeicon").style.zIndex = "";
-    document.getElementById("loupeicon").style.position = "";
-    document.getElementById("loupeicon").style.backgroundColor = "";
-}
-
-
-
 var imgslider = document.getElementsByClassName("img-slide");
 document.getElementById("slide1").addEventListener("click", () => {
     $(".img-slide").css("opacity", "0");
@@ -46,4 +22,4 @@ setInterval(() => {
     if (index == 3) {
         index = 0;
     }
-}, 3000)
+}, 5000)
