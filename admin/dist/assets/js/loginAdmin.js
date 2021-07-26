804c129a479ba210e3676827b95ef4281d48fdd3
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-07-27 00:08:16 +0700

    update

---
+++ admin/dist/assets/js/loginAdmin.js
@@ -0,0 +1,88 @@
+
+var listAccountAdmin = localStorage.getItem("userAdmin")
+  ? JSON.parse(localStorage.getItem("userAdmin"))
+  : [];
+var userCurrentAdmin = localStorage.getItem("userCurrentAdmin") ? (localStorage.getItem("userCurrentAdmin")) : "";
+var userCurrentAdmin2 = localStorage.getItem("userCurrentAdmin2") ? (localStorage.getItem("userCurrentAdmin2")) : "";
+
+userCurrentAdmin ? document.getElementById("nameCurrentAdmin").innerHTML = userCurrentAdmin : null;
+userCurrentAdmin2 ? document.getElementById("emailCurrentAdmin").innerHTML = userCurrentAdmin2 : null;
+userCurrentAdmin ? document.getElementById("nameCurrentAdmin2").innerHTML = userCurrentAdmin : null;
+userCurrentAdmin2 ? document.getElementById("emailCurrentAdmin2").innerHTML = userCurrentAdmin2 : null;
+
+function checkLoginAdmin() {
+  var cEmailLogin = document.getElementById("emailLogin").value;
+  var cPassLogin = document.getElementById("passLogin").value;
+  var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+    String(cEmailLogin).toLowerCase()
+  );
+  if (cEmailLogin != "" || cPassLogin != "" || cEmailLogin.match(emailFormat)) {
+    listAccountAdmin.forEach((userAdmin) => {
+      if (
+        userAdmin.email == cEmailLogin &&
+        userAdmin.password == cPassLogin &&
+        userAdmin.role == 1
+      ) {
+        Swal.fire("Good job!", "You clicked the button!", "success");
+        localStorage.setItem("userCurrentAdmin", userAdmin.userName);
+        localStorage.setItem("userCurrentAdmin2", userAdmin.email);
+        loginD.setAttribute("href", "/admin/dist/index.html");
+        return;
+      }
+    });
+    document.getElementById("checkError").innerHTML =
+      "Bạn đã nhập sai thông tin vui lòng nhập lại!";
+  } else {
+    document.getElementById("checkError").innerHTML =
+      "Vui lòng nhập đúng định dạng!";
+  }
+}
+
+function checkRegister() {
+  var cNameRegister = document.getElementById("cNameRegister").value;
+  var cEmailRegister = document.getElementById("cEmailRegister").value;
+  var cPassRegister = document.getElementById("cPassRegister").value;
+  var cPassRegisterC = document.getElementById("cPassRegisterC").value;
+  var emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+    String(cEmailRegister).toLowerCase()
+  );
+
+  if (
+    cNameRegister != "" ||
+    cEmailRegister != "" ||
+    cPassRegister != "" ||
+    cPassRegisterC != "" ||
+    cEmailRegister.match(emailFormat)
+  ) {
+    if (listAccountAdmin.some((userAdmin) => userAdmin.email === cEmailRegister)) {
+      document.getElementById("checkError2").innerHTML =
+        "Tài khoản đã tồn tại!";
+      return;
+    } else {
+      if (cPassRegister != cPassRegisterC){
+        document.getElementById("checkError2").innerHTML =
+      "Xác nhận mật khẩu không đúng!";
+      }else {
+        var userAdmin = {
+          userName: cNameRegister,
+          email: cEmailRegister,
+          password: cPassRegister,
+          role: 1
+        };
+        listAccountAdmin.push(userAdmin);
+        localStorage.setItem("userAdmin", JSON.stringify(listAccountAdmin));
+        Swal.fire("ĐĂNG KÝ THÀNH CÔNG!", "Click ok để xác nhận!", "Ok");
+        console.log(listAccountAdmin);
+      }
+    }
+  } else {
+    document.getElementById("checkError2").innerHTML =
+      "Vui lòng nhập đúng định dạng!";
+  }
+}
+
+
+function logout() {
+  localStorage.setItem("userCurrentAdmin", "")
+  localStorage.setItem("userCurrentAdmin2", "")
+}
