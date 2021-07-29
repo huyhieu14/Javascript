eb592c0b3a13fc3792bf1be28806e694a5bf2fe0
huyhieu14 <83973708+huyhieu14@users.noreply.github.com>
2021-07-30 02:59:59 +0700

    u

---
+++ admin/dist/assets/js/listAcount.js
@@ -0,0 +1,65 @@
+var users = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : [];
+
+
+users.map((users, index) => {
+  listAcount.insertAdjacentHTML(
+    "beforeend",
+    `
+        <tr>
+            <td>${index}</td>
+            <td>${users.userName}</td>
+            <td>${users.email}</td>
+            <td>+84</td>
+            <td><input type="button" class="btn btn-checkRole text-white" value="ADMIN"></td>
+            <td><input type="button" class="btn btn-danger text-dark btn-delete" data-id=${index} value="XOÁ"> <input type="button" class="btn btn-warning btn-reset" data-id=${index} value="RESET" title="RESET MẬT KHẨU 1->8"></td>
+        </tr>
+        `
+  );
+    switch(users.role) {
+        case 1: 
+            var z = document.getElementsByClassName("btn-checkRole")[index]
+            z.style.backgroundColor = "black";
+            z.setAttribute("value", "ADMIN");
+        break;
+        case 0:
+            var z = document.getElementsByClassName("btn-checkRole")[index]
+            z.style.backgroundColor = "#00DD00";
+            z.setAttribute("value", "USER");
+        break;
+  }
+});
+
+function deleteAcount(){
+    document.querySelectorAll(".btn-delete").forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            e.target.parentNode.remove();
+            var id = e.target.getAttribute("data-id");
+            users.splice(id, 1);
+            localStorage.setItem("user", JSON.stringify(users));
+            window.location.reload();
+        })
+    })
+}
+deleteAcount();
+
+function resetPass(){
+    document.querySelectorAll(".btn-reset").forEach(btn => {
+        btn.addEventListener("click", (e) => {
+            var id = e.target.getAttribute("data-id");
+            var user = {
+                ...users[id],
+                password: 12345678,
+            }
+            users[id] = user;
+            localStorage.setItem("user", JSON.stringify(users))
+            Swal.fire("Reset thành công!", "Click để xác nhận!", "Ok");
+        }) 
+    })
+}
+resetPass();
+
+
+
+
--- assets/js/store.js
+++ assets/js/store.js
@@ -1,6 +1,7 @@
 var carts = localStorage.getItem("carts")
   ? JSON.parse(localStorage.getItem("carts"))
   : [];
+  
 function sliderStore() {
   var indexSlider = 1;
   setInterval(() => {
@@ -23,14 +24,11 @@ const getParameterByName = (name, url = window.location.href) => {
 };
 
 const page = getParameterByName("page") || 1;
-
 var arrProduct = [];
 fetch("http://localhost:3000/product")
   .then((res) => res.json())
   .then((data) => {
     arrProduct = [...data].reverse();
-    // card(arrProduct);
-    
     var perPage = 8;
     var start = (page - 1) * perPage;
     var end = page * perPage;
@@ -39,12 +37,7 @@ fetch("http://localhost:3000/product")
     for (let i = 1; i <= totalPages; i++) {
       tempArr[i] = i;
     }
-
     arrProduct.slice(start, end).map((card) => {
-      console.log(arrProduct.slice(start, end));
-      console.log(arrProduct);
-      console.log(start);
-      console.log(end);
       product.insertAdjacentHTML(
         "beforeend",
         `
@@ -91,87 +84,7 @@ fetch("http://localhost:3000/product")
       );
     });
     addEventButton();
-    // document.querySelectorAll(".pagination-button").forEach((item) => {
-    //   item.addEventListener("click", (e) => {
-    //     document.getElementById("product").innerHTML = "";
-    //     page = e.target.parentNode.getAttribute("pagination-id");
-    //     start = (page - 1) * perpage;
-    //     end = page * perpage;
-    //     arrProduct.slice(start, end).reverse().map((card) => {
-    //       product.insertAdjacentHTML(
-    //         "afterbegin",
-    //         `
-    //                 <div class="col-xl-3 col-md-6 mt-3" data-id=${card.id}>
-    //                     <div class="card">
-    //                         <img class="card-img-top align-self-center" src=${card.image} alt="Card image cap" style="width: 160px; height: 160px;">
-    //                         <div class="card-body">
-    //                             <h5 class="card-title border-bottom fw-bold">${card.title}</h5>
-    //                             <div class="card-text border-bottom">
-    //                                 <ul>
-    //                                     <li class="card-system">${card.system}
-    //                                     </li>
-    //                                     <li class="card-core">${card.core}
-    //                                     </li>
-    //                                     <li class="card-vga">${card.vga}
-    //                                     </li>
-    //                                     <li class="card-ram">${card.ram}
-    //                                     </li>
-    //                                     <li class="card-storage">${card.storage}
-    //                                     </li>
-    //                                 </ul>
-    //                             </div>
-    //                             <div class="mt-3">
-    //                                 <span class="float-start fs-4 fw-bold">
-    //                                 $<span class="card-price">${card.price}</span>
-    //                                 </span>
-    //                                 <button data-id=${card.id} class="btn btn-add float-end">Thêm giỏ hàng </button>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 </div>`
-    //       );
-    //     });
-    //   });
-    // });
-  });
-
-// function card(cards) {
-//   var html = cards.map((card) => {
-//     product.insertAdjacentHTML(
-//       "afterbegin",
-//       `
-//             <div class="col-xl-3 col-md-6 mt-3">
-//                 <div class="card">
-//                     <img class="card-img-top align-self-center" src=${card.image} alt="Card image cap" style="width: 160px; height: 160px;">
-//                     <div class="card-body">
-//                         <h5 class="card-title border-bottom fw-bold">${card.title}</h5>
-//                         <div class="card-text border-bottom">
-//                             <ul>
-//                                 <li class="card-system">${card.system}
-//                                 </li>
-//                                 <li class="card-core">${card.core}
-//                                 </li>
-//                                 <li class="card-vga">${card.vga}
-//                                 </li>
-//                                 <li class="card-ram">${card.ram}
-//                                 </li>
-//                                 <li class="card-storage">${card.storage}
-//                                 </li>
-//                             </ul>
-//                         </div>
-//                         <div class="mt-3">
-//                             <span class="float-start fs-4 fw-bold">
-//                             $<span class="card-price">${card.price}</span>
-//                             </span>
-//                             <button data-id=${card.id} class="btn btn-add float-end">Thêm giỏ hàng </button>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>`
-//     );
-//   });
-//   return html;
-// }
+});
 
 function increaseCount(a, b) {
   var input = b.previousElementSibling;
@@ -211,7 +124,19 @@ const addProduct = (e) => {
   let price = parent.getElementsByClassName("card-price")[0].innerText;
   let total = price;
   let value = 1;
-  let card = { id, image, title, system, core, vga, ram, storage, price, total, value};
+  let card = {
+    id,
+    image,
+    title,
+    system,
+    core,
+    vga,
+    ram,
+    storage,
+    price,
+    total,
+    value,
+  };
   if (carts.some((card) => card.id === id)) {
     console.log("Đã có trong giỏ hàng");
     return;
